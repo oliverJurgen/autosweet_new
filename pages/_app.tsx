8033029c51269d107be8a9baadef83d881357574
oliverJurgen <oliver.jurgen90@gmail.com>
2021-12-16 04:59:39 -0500

    Import css files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import "../styles/globals.css";
+import "../styles/index.css";
+import "../styles/Router.css";
+import "../node_modules/antd/dist/antd.css";
+import "../components/SearchArea/SearchArea.module.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
