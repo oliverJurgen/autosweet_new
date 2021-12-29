9ee5b40e6a6bfa4d93c7bf17a7133340299ec3a8
volodymyr.b <volodymyr.b@scopicsoftware.com>
2021-12-30 00:15:27 +0200

    HomePage rebuild and QuickLinks integration

--- old_pages/HomePage/HomePage.js
+++ old_pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { compose } from "redux";
+import React from 'react';
+import { compose } from 'redux';
 // import { withRouter } from 'react-router-dom';
-import { withRouter } from "next/router";
-import { connect } from "react-redux";
-import SearchArea from "../../components/SearchArea";
+import { withRouter } from 'next/router';
+import Link from 'next/link';
+import Image from 'next/image';
+import { connect } from 'react-redux';
+import SearchArea from '../../components/SearchArea';
 import {
   getTags,
   performSearchAction,
@@ -13,10 +15,12 @@ import {
   setLat,
   removeTag,
   addTag,
-} from "../../redux/actions";
-import { getSearchValue, getSelectedTags } from "../../redux/selectors";
-import Navigation from "../../components/Navigation";
-
+} from '../../redux/actions';
+import { getSearchValue, getSelectedTags } from '../../redux/selectors';
+import Navigation from '../../components/Navigation';
+import style from '../../styles/modules/HomePage.module.css';
+import Logo from '../../public/assets/img/icons/AutosweetAUTOS_Final-1png-03.png';
+import Footer from '../../components/Footer';
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
@@ -43,13 +47,13 @@ class HomePage extends React.Component {
   runSearch(value) {
     this.props.changeResultPageAction(1);
     this.props.router.push(
-      "/search-result?q=" +
+      '/search-result?q=' +
         value +
-        "&page=1" +
-        "&tags=" +
-        "&lat=" +
+        '&page=1' +
+        '&tags=' +
+        '&lat=' +
         this.props.lat +
-        "&lon=" +
+        '&lon=' +
         this.props.lon
     );
   }
@@ -71,26 +75,126 @@ class HomePage extends React.Component {
       }
     }
   }
-
+  hardcodeData = {
+    condition: [
+      {
+        name: 'New Cars',
+        count: '10,000',
+        price: '10,000',
+      },
+      {
+        name: 'Used Cars',
+        count: '10,000',
+        price: '10,000',
+      },
+    ],
+    brands: [
+      {
+        name: 'BMW',
+        count: '11,278',
+      },
+      {
+        name: 'Audi',
+        count: '11,278',
+      },
+      {
+        name: 'Honda',
+        count: '11,278',
+      },
+      {
+        name: 'Toyota',
+        count: '11,278',
+      },
+      {
+        name: 'Ford',
+        count: '11,278',
+      },
+      {
+        name: 'Mercedes-Benz',
+        count: '11,278',
+      },
+      {
+        name: 'Hyundai',
+        count: '11,278',
+      },
+      {
+        name: 'GMC',
+        count: '11,278',
+      },
+    ],
+  };
   render() {
     const { tags, searchValue, selectedTags } = this.props;
     return (
-      <div className="home-grid">
-        <header className="homeHeader">
+      <>
+        <header className={style.header}>
+          <Link href="/">
+            <picture>
+              <Image src={Logo} alt="logo" className={style.logo} />
+            </picture>
+          </Link>
           <Navigation />
         </header>
-        <div className="logoHome" />
-        <SearchArea
-          dark={true}
-          tags={tags}
-          selectedTags={selectedTags}
-          onSearch={this.runSearch}
-          searchValue={searchValue}
-          onSearchChange={this.searchTermChange}
-          onRemoveTag={this.removeTag}
-          onToggleTag={this.toggleTag}
-        />
-      </div>
+        <main>
+          <section className={style.searchSection}>
+            <div>
+              <SearchArea
+                dark={true}
+                tags={tags}
+                selectedTags={selectedTags}
+                onSearch={this.runSearch}
+                searchValue={searchValue}
+                onSearchChange={this.searchTermChange}
+                onRemoveTag={this.removeTag}
+                onToggleTag={this.toggleTag}
+              />
+            </div>
+          </section>
+          <section className={style.quickLinkSection}>
+            <article className={style.linkArticle}>
+              <header>
+                <p className={style.linkArticleHeader}>
+                  <b>Condition</b>
+                </p>
+              </header>
+              <div className={style.linkCardsBlock}>
+                {this.hardcodeData.condition.map((item) => (
+                  <Link href={`/search-result?q=${item.name}&page=1`}>
+                    <div className={style.linkCard}>
+                      <h5 className={style.linkCardHeader}>{item.name}</h5>
+                      <p className={style.linkCardBody}>
+                        {item.count} Listings
+                        <br />
+                        Start from ${item.price}
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </article>
+            <article className={style.linkArticle}>
+              <header>
+                <p className={style.linkArticleHeader}>
+                  <b>Brands</b>
+                </p>
+              </header>
+              <div className={style.linkCardsBlock}>
+                {this.hardcodeData.brands.map((item) => (
+                  <Link href={`/search-result?q=${item.name}&page=1`}>
+                    <div className={style.linkCard}>
+                      <h5 className={style.linkCardHeader}>{item.name}</h5>
+                      <p className={style.linkCardBody}>
+                        {item.count} Listings
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </article>
+          </section>
+        </main>
+        <Footer />
+      </>
     );
   }
 }
