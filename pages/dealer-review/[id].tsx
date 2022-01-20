c8c869872faf7c2a8442518621180f83be49fac1
volodymyr.b <volodymyr.b@scopicsoftware.com>
2022-01-20 12:11:36 +0200

    AUT-243

--- pages/dealer-review/[id].tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from "next";
-import DealerReviewsPage from '../../old_pages/DealerReviewsPage';
-const DealerReview: NextPage = () => {
-  return <DealerReviewsPage/>;
-};
-
-export default DealerReview;
