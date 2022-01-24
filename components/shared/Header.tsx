ae4da308bc6914dea31c310e8700479490f743e8
oliverJurgen <oliver.jurgen90@gmail.com>
2022-01-24 17:03:04 -0500

    remove next image

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { chakra as c, Flex } from "@chakra-ui/react";
+import { chakra as c, Image, Flex } from "@chakra-ui/react";
 import Navigation from "components/Navigation";
 import Logo from "/public/assets/img/icons/AutosweetAUTOS_Final-1png-03.png";
-import Image from "next/image";
 
 const Header = () => {
   return (
@@ -16,13 +15,7 @@ const Header = () => {
       <Link href="/">
         <c.div cursor="pointer">
           <picture>
-            <Image
-              width="150px"
-              height="150px"
-              objectFit="contain"
-              src={Logo.src}
-              alt="logo"
-            />
+            <Image w="150px" src={Logo.src} alt="logo" />
           </picture>
         </c.div>
       </Link>
