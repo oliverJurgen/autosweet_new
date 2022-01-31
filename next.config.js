f33e08e3eeac34e1775d2a784ee7ae144ddcb484
oliverJurgen <oliver.jurgen90@gmail.com>
2022-01-31 09:01:45 -0500

    remove NODE_ENV in next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = withTm(
   withVideos({
     reactStrictMode: true,
     env: {
-      NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+      // NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
       REACT_APP_BASE_URL: process.env.NEXT_PUBLIC_REACT_APP_BASE_URL,
     },
     images: {
